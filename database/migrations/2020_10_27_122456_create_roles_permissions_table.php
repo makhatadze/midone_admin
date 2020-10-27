5b8cc94c2a7b4d5fd25b86d95fcdacd5017565f0
makhatadze <vitomaxatadze@gmail.com>
2020-10-27 16:46:16 +0400

    Create roles and permissions

---
+++ database/migrations/2020_10_27_122456_create_roles_permissions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles_permissions', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->bigInteger('role_id')->unsigned()->index()->nullable();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+
+
+
+            $table->primary(['user_id','role_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles_permissions');
+    }
+}
