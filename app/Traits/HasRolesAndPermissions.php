4df15c32b0c5cff697e64ad836942fa92bac56ac
makhatadze <vitomaxatadze@gmail.com>
2020-10-28 15:56:49 +0400

    Add traits for roles and permission system.

---
+++ app/Traits/HasRolesAndPermissions.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *  app/Traits/HasRolesAndPermissions.php
+ *
+ * User:
+ * Date-Time: 28.10.20
+ * Time: 10:16
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+namespace App\Traits;
+
+use App\Models\Role;
+
+trait HasRolesAndPermissions
+{
+    /**
+     * @return boolean
+     */
+    public function isAdmin()
+    {
+        return $this->roles->contains('slug', 'admin');
+    }
+
+    /**
+     *
+     * @return mixed
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'users_roles');
+    }
+
+    /**
+     *
+     * @return mixed
+     */
+    public function permissions()
+    {
+        return $this->belongsToMany(Role::class, 'users_roles');
+    }
+
+    /**
+     * Check if the user has Role
+     *
+     * @param [type] $role
+     *
+     * @return boolean
+     */
+    public function hasRole($role)
+    {
+        if (strpos($role, ',') !== false) {//check if this is an list of roles
+
+            $listOfRoles = explode(',', $role);
+
+            foreach ($listOfRoles as $role) {
+                if ($this->roles->contains('slug', $role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->roles->contains('slug', $role)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
