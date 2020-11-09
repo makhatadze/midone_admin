5aea2218231c78a7f9bc8fbae6d7775afee47f0a
makhatadze <vitomaxatadze@gmail.com>
2020-11-09 12:31:20 +0400

    Add mailer.

---
+++ app/Mail/TicketMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TicketMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('test@test.ge', $this->data['user'])
+            ->subject($this->data['subject'])
+            ->view('backend.mail.create', ['data' => $this->data]);
+    }
+}
