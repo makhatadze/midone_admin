c2ce60c1fe66e7ef842d455a1bee86e02d7cb365
makhatadze <vitomaxatadze@gmail.com>
2021-02-10 16:28:37 +0400

    Add cluster.

--- resources/js/socket/app.js
+++ resources/js/socket/app.js
@@ -4,11 +4,13 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'qwerty',
-    wsHost: window.location.hostname,
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    wsHost: '127.0.0.1',
     wsPort: 6001,
     forceTLS: false,
     disableStats: true,
+    enabledTransports: ['ws', 'wss']
 });
 
 
