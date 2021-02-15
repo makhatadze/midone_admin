9c1ac955b5ca47d6c61e475d51618238ef6c2a37
makhatadze <vitomaxatadze@gmail.com>
2021-02-15 14:54:58 +0400

    Add admin roles to user.

--- resources/views/backend/layout/base.blade.php
+++ resources/views/backend/layout/base.blade.php
@@ -13,13 +13,8 @@ License: You must have a valid license purchased only from themeforest(the above
 <head>
     <meta charset="utf-8">
     <link href="{{ asset('logo.svg') }}" rel="shortcut icon">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description"
-          content="Midone admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
-    <meta name="keywords"
-          content="admin template, Midone admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="LEFT4CODE">
+
 
 @yield('head')
 
