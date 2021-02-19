7c00ccb0c0d35de6c80bc6a423ecbc29613f7c70
makhatadze <vitomaxatadze@gmail.com>
2021-02-19 12:54:14 +0400

    Change timezone.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191); //NEW: Increase StringLength
-
+        date_default_timezone_set('Asia/Tbilisi');
     }
 }
