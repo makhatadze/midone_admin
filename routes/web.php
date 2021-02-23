698c74c2fbc4c5911b97a4cf0f4d7cda7e219b68
Zarna0x <justnightmare123@gmail.com>
2021-02-23 14:22:02 -0800

    add route for ticket export log

--- routes/web.php
+++ routes/web.php
@@ -110,7 +110,7 @@ Route::prefix('admin')->group(function () {
         
         Route::get('tickets/export',[TicketsController::class,'exportToExcel'])->name('exportToExcel');
 
-
+        Route::get('tickets/export-log',[TicketsController::class,'exportLog'])->name('ticketExportLog');
     });
 
 });
