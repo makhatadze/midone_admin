cad194fde0b1ad6c8d412dd60d312323cd61d1bb
Zarna0x <justnightmare123@gmail.com>
2021-02-26 10:13:09 -0800

    add trait to validate filter values

---
+++ app/Traits/TicketFilters.php
@@ -0,0 +1,33 @@
+<?php
+
+   namespace App\Traits;
+
+   trait TicketFilters
+   {
+       protected $filters = [
+        'filter-ticket-status' => 'status',
+        'filter-ticket-department' => 'department'
+       ];
+       
+       public function validateFilter(string $filter, $value)
+       {
+           if (!array_key_exists($filter, $this->filters) || empty($value)) {
+               return false;
+           }
+           
+           // Reflection Method Can be used too, but not needed since we already know method already exists.
+           return $this->{$this->filters[$filter]}($value);
+       }
+       
+       public function department ($ticketStatus) 
+       {
+           return true;
+       }
+       
+       public function status ( $ticketStatus )
+       {
+           return in_array($ticketStatus,['success','pending','closed']);
+       }
+       
+   }
+   
