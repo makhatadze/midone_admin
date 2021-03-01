174adc03851852e6d945655183442e76c0b7c40f
Zarna0x <justnightmare123@gmail.com>
2021-03-01 15:05:17 -0800

    refactor code to merge additional ticket departments with default ones

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -1,113 +1,139 @@
 <?php
 
-namespace App\Models;
+   namespace App\Models;
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+   use Illuminate\Support\Facades\DB;
+   use Illuminate\Database\Eloquent\Factories\HasFactory;
+   use Illuminate\Database\Eloquent\Model;
 
-class Ticket extends Model
-{
-    use HasFactory;
+   class Ticket extends Model
+   {
 
-    protected $fillable = [
-        'user_id',
-        'department_id',
-        'category_id',
-        'name',
-        'level',
-        'deadline',
-        'confirm',
-        'process',
-        'closed_at',
-    ];
-    protected $table = 'tickets';
+       use HasFactory;
 
-    public function department()
-    {
-        return $this->hasOne('App\Models\Department', 'id', 'department_id');
-    }
+       protected $fillable = [
+           'user_id',
+           'department_id',
+           'category_id',
+           'name',
+           'level',
+           'deadline',
+           'confirm',
+           'process',
+           'closed_at',
+       ];
+       protected $table = 'tickets';
 
-    // Message
-    public function message()
-    {
-        return $this->morphMany('App\Models\Message', 'messageable');
-    }
+       public function department()
+       {
+           return $this->hasOne('App\Models\Department', 'id', 'department_id');
+       }
 
-    public function getTicketLevelName()
-    {
-        if ($this->level == 1)
-            return 'Low';
-        if ($this->level == 2)
-            return 'Medium';
-        if ($this->level == 3)
-            return 'High';
-        return false;
-    }
+       // Message
+       public function message()
+       {
+           return $this->morphMany('App\Models\Message', 'messageable');
+       }
 
-    public function getApproveDepartments()
-    {
-        $data = [];
+       public function getTicketLevelName()
+       {
+           if ($this->level == 1)
+               return 'Low';
+           if ($this->level == 2)
+               return 'Medium';
+           if ($this->level == 3)
+               return 'High';
+           return false;
+       }
 
-        // Get if Main Department approved
-        $data[0] = $this->getApproveArray($this->department_id);
+       public function getApproveDepartments()
+       {
 
-        if ($this->category_id) {
-            $category = Category::find($this->category_id);
-            if (count($category->departments) > 0) {
-                foreach ($category->departments as $dep) {
-                    $data [] = $this->getApproveArray($dep->id);
-                }
-            }
-        }
-        return $data;
-    }
+           $data = [];
+           // Get if Main Department approved
+           $data[0] = $this->getApproveArray($this->department_id);
 
-    protected function getApproveArray($department_id)
-    {
-        $approveDepartment = Approve::where([['department_id', $department_id], ['ticket_id', $this->id]])->first();
-        if ($approveDepartment != null) {
-            return [
-                'approved' => true,
-                'user' => User::getName($approveDepartment->user_id),
-                'department' => Department::getName($approveDepartment->department_id),
-                'created_at' => $approveDepartment->created_at,
-                'status' => $approveDepartment->status,
-                'department_id' => $department_id
-            ];
-        }
-        return [
-            'approved' => false,
-            'user' => '',
-            'department' => Department::getName($department_id),
-            'created_at' => '',
-            'department_id' => $department_id
-        ];
-    }
+           if ($this->category_id) {
+               $category = Category::find($this->category_id);
+               if (count($category->departments) > 0) {
+                   foreach ($category->departments as $dep) {
+                       $data [] = $this->getApproveArray($dep->id);
+                   }
+               }
+           }
+           // Get Additional Departments
 
-    public function user()
-    {
-        return $this->hasone('App\Models\User', 'id', 'user_id');
+           $additionalDepartmentsModel = $this->additionalDepartments;
 
-    }
-    
-    public function scopeClosed($query)
-    {
-        return $query->whereNotNull('closed_at');
-    }
-     
-    public function scopeSuccess($query)
-    {
-        return $query->where(DB::raw('closed_at'), '<=', DB::raw('deadline'));
-    }
-     
-    public function scopePending($query)
-    {
-        return $query->whereNull('closed_at');        
-    }
-     
-    public function scopeDepartment($query,$departmentId)
-    {
-        return $query->where('department_id','=',$departmentId);
-    }
-}
+           // Return default departments if we dont have any additional ones.
+           if (!$additionalDepartmentsModel) {
+               return $data;
+           }
+
+           $additionalDepartmentIds = unserialize($additionalDepartmentsModel->additional_departments);
+            
+           // Get Unique Arrays
+           $leftDeparmentIds = array_diff($additionalDepartmentIds, array_column($data,'department_id'));
+
+           // add additional departments
+           foreach ($leftDeparmentIds as $depId) {
+               $data[] = $this->getApproveArray($depId);
+           }
+
+           return $data;
+       }
+
+       protected function getApproveArray($department_id)
+       {
+           $approveDepartment = Approve::where([['department_id', $department_id], ['ticket_id', $this->id]])->first();
+           if ($approveDepartment != null) {
+               return [
+                   'approved' => true,
+                   'user' => User::getName($approveDepartment->user_id),
+                   'department' => Department::getName($approveDepartment->department_id),
+                   'created_at' => $approveDepartment->created_at,
+                   'status' => $approveDepartment->status,
+                   'department_id' => $department_id
+               ];
+           }
+           return [
+               'approved' => false,
+               'user' => '',
+               'department' => Department::getName($department_id),
+               'created_at' => '',
+               'department_id' => $department_id
+           ];
+       }
+
+       public function user()
+       {
+           return $this->hasone('App\Models\User', 'id', 'user_id');
+       }
+
+       public function additionalDepartments()
+       {
+           return $this->hasOne(TicketDepartments::class, 'ticket_id', 'id');
+       }
+
+       public function scopeClosed($query)
+       {
+           return $query->whereNotNull('closed_at');
+       }
+
+       public function scopeSuccess($query)
+       {
+           return $query->where(DB::raw('closed_at'), '<=', DB::raw('deadline'));
+       }
+
+       public function scopePending($query)
+       {
+           return $query->whereNull('closed_at');
+       }
+
+       public function scopeDepartment($query, $departmentId)
+       {
+           return $query->where('department_id', '=', $departmentId);
+       }
+
+   }
+   
