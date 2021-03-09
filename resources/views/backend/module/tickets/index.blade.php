af4d7d705e240f10e83e1c5f901a83037be819cd
makhatadze <vitomaxatadze@gmail.com>
2021-03-09 10:04:40 +0400

    Change ticket levet title.

--- resources/views/backend/module/tickets/index.blade.php
+++ resources/views/backend/module/tickets/index.blade.php
@@ -327,7 +327,7 @@
                             <option value="">Select level</option>
                             <option value="1">Low</option>
                             <option value="2">Medium</option>
-                            <option value="3">Hight</option>
+                            <option value="3">High</option>
                         </select>
                     </div>
                     <span class="help-block help-ticket-level mt-1">
